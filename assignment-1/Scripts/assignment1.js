3ee87e516078a8e766754146a18eedd45c7bb339
Isaku <isaialmeraz2003@gmail.com>
2025-02-25 17:42:30 -0800

    Assignment 1

---
+++ assignment-1/Scripts/assignment1.js
@@ -0,0 +1,28 @@
+// 1. Declare Variables
+// Using let
+let character = "Tom";
+let action = "laugh";
+let object = "a talking pineapple";
+let place = "the library";
+let time = "a stormy night";
+
+// Using const
+const occasion = "a very wild dream";
+
+// 2. Create the Story
+console.log("--- Funny Story ---");
+console.log(character + " was at " + place + " on " + time + ".");
+console.log("Suddenly, " + character + " saw " + object + " and started to " + action + ".");
+console.log("It turned out to be " + occasion + " and then he woke up!");
+
+// 3. Experiment with Different Values
+character = "Lucy";
+action = "point";
+object = "a floating sandwich";
+place = "the amusement park";
+time = "a bright morning";
+
+console.log("--- Different Version of the Story ---");
+console.log(character + " was at " + place + " on " + time + ".");
+console.log("Suddenly, " + character + " saw " + object + " and decided to " + action + "at it.");
+console.log("It turned out to be " + occasion + " and everyone had a blast!");
